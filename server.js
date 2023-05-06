92ec7f7aa28d28a5f9ba1a62f04219e841e4d795
trandung2k1 <trandungitksnb2001@gmail.com>
2023-05-06 07:39:58 +0700

    disable cors

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const port = 4000;
 const app = express();
-app.use(cors());
+// app.use(
+//     cors({
+//         origin: 'http://127.0.0.1:5500/client/index.html',
+//     }),
+// );
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.get('/', (req, res) => {
